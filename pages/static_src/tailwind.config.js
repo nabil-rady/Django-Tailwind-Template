f06964348f72d666c3f7018536ffa53b9a1666ba
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-09-15 23:36:19 +0200

    Inital Commit

---
+++ pages/static_src/tailwind.config.js
@@ -0,0 +1,110 @@
+/**
+ * This is a minimal config.
+ *
+ * If you need the full config, get it from here:
+ * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ */
+
+module.exports = {
+  /**
+   * Stylesheet generation mode.
+   *
+   * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
+   * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
+   */
+  mode: "jit",
+
+  purge: [
+    /**
+     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+     */
+
+    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+    '../templates/**/*.html',
+
+    /* 
+      * Main templates directory of the project (BASE_DIR/templates).
+      * Adjust the following line to match your project structure.
+      */
+    '../../templates/**/*.html',
+    
+    /* 
+      * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+      * Adjust the following line to match your project structure.
+      */
+    '../../**/templates/**/*.html',
+
+    /**
+     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+     * patterns match your project structure.
+     */
+    /* JS 1: Ignore any JavaScript in node_modules folder. */
+    // '!../../**/node_modules',
+    /* JS 2: Process all JavaScript files in the project. */
+    // '../../**/*.js',
+
+    /**
+     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+     * and make sure the pattern below matches your project structure.
+     */
+    // '../../**/*.py'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: '#E6F6FE',
+          200: '#C0EAFC',
+          300: '#9ADDFB',
+          400: '#4FC3F7',
+          500: '#03A9F4',
+          600: '#0398DC',
+          700: '#026592',
+          800: '#014C6E',
+          900: '#013349',
+        },
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+      },
+      lineHeight: {
+        hero: '4.5rem',
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
